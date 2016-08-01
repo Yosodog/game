d267ca200d45c583e1c249c7ecd6c1745c94bc66
Yosodog <yosodog@gmail.com>
2016-08-01 03:43:06 -0400

    Base city table migration

---
+++ database/migrations/2016_08_01_073941_base_city_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class BaseCityTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('cities', function (Blueprint $table) {
+            $table->increments('id')->unsigned();
+            $table->timestamps();
+            $table->integer("nation_id")->unsigned();
+            $table->string("name");
+            $table->integer("population")->unsigned()->default(500000);
+
+            $table->foreign("nation_id")->references('id')->on('nations');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop("cities");
+    }
+}
