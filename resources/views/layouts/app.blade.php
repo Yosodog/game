9c3a7a7cf4b2e39fe89b2c8307691e951974a453
Yosodog <yosodog@gmail.com>
2016-08-01 04:15:31 -0400

    Displaying cities in dropdown on main mav

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -53,7 +53,15 @@
 
                                 <ul class="dropdown-menu" role="menu">
                                     <li><a href="{{ url("/nation/view/".Auth::user()->nation->id) }}">View Nation</a></li>
-                                    <li><a href="{{ url("#") }}">Cities</a></li>
+                                    <li class="dropdown"><a href="{{ url("#") }}">Cities <i class="fa fa-caret-right" aria-hidden="true"></i></a>
+                                        <ul class="dropdown-menu">
+                                            <li><a href="{{ url("#") }}">View All</a></li>
+                                            <li class="divider"></li>
+                                            @foreach ($nation->cities as $city)
+                                                <li><a href="{{ url("cities/view/".$city->id) }}">{{ $city->name }}</a></li>
+                                            @endforeach
+                                        </ul>
+                                    </li>
                                 </ul>
                             </li>
                             <li class="dropdown">
