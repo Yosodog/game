6b578378331b139b6cb893cf944ff75301cdc900
Yosodog <yosodog@gmail.com>
2016-08-17 22:01:12 -0400

    Added migration to add building time to building_types table

---
+++ database/migrations/2016_08_18_015936_add_build_time_to_buildings_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class AddBuildTimeToBuildingsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('building_types', function (Blueprint $table) {
+            $table->smallInteger("buildingTime")->unsigned();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('building_types', function (Blueprint $table) {
+            $table->dropColumn("buildingTime");
+        });
+    }
+}
