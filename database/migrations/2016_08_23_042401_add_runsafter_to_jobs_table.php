37778573234c074b8fc23ee29c19e75de169e1ef
Yosodog <yosodog@gmail.com>
2016-08-23 00:29:43 -0400

    Added runsAfter column to jobs migration and buildings table migration

---
+++ database/migrations/2016_08_23_042401_add_runsafter_to_jobs_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class AddRunsafterToJobsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table("jobs", function(Blueprint $table) {
+           $table->integer("runsAfter")->unsigned()->comment("The ID of the job that this job will run after");
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table("jobs", function(Blueprint $table) {
+            $table->dropColumn("runsAfter");
+        });
+    }
+}
---
+++ database/migrations/2016_08_23_042415_add_buildings_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class AddBuildingsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create("buildings", function(Blueprint $table) {
+            $table->increments("id");
+            $table->timestamps();
+            $table->integer("city_id")->unsigned();
+            $table->integer("building_id")->unsigned();
+            $table->smallInteger("quantity")->unsigned();
+
+            $table->foreign("city_id")->references('id')->on('cities');
+            $table->foreign("building_id")->references('id')->on('building_types');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropTable("buildings");
+    }
+}
