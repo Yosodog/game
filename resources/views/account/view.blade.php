102763c50af74853864807d452b39a45a32cb88c
Yosodog <yosodog@gmail.com>
2016-11-07 18:18:28 -0500

    Changed account edit interface (#6)

--- resources/views/account/view.blade.php
+++ resources/views/account/view.blade.php
@@ -2,46 +2,49 @@
 
 @section('content')
     @include("alerts") {{-- Include the template for alerts. This checks if there's something needed to display --}}
-    <h1 class="text-center">Edit Account</h1>
     <div class="row">
-        <div class="col-md-6">
-            <div class="panel panel-info">
+        <div class="col-md-3">
+            @include("account.accountNav")
+        </div>
+        <div class="col-md-9">
+            <div class="panel panel-default">
                 <div class="panel-heading">Change Email</div>
                 <div class="panel-body">
-                    <form method="post" action="{{ url("/account/edit/email") }}">
+                    <div class="col-md-6">
+                        <form method="post" action="{{ url("/account/edit/email") }}">
 
-                        <div class="form-group">
-                            <label for="email">Email</label>
-                            <input type="email" id="email" name="email" class="form-control" placeholder="{{ $user->email }}" required>
-                        </div>
+                            <div class="form-group">
+                                <label for="email">Email</label>
+                                <input type="email" id="email" name="email" class="form-control" placeholder="{{ $user->email }}" required>
+                            </div>
 
-                        <div class="form-group">
-                            {{ csrf_field() }}
-                            {{ method_field("PATCH") }}
-                            <input type="submit" value="Edit" class="btn btn-info">
-                        </div>
-                    </form>
+                            <div class="form-group">
+                                {{ csrf_field() }}
+                                {{ method_field("PATCH") }}
+                                <input type="submit" value="Edit" class="btn btn-default">
+                            </div>
+                        </form>
+                    </div>
                 </div>
             </div>
-        </div>
-
-        <div class="col-md-6">
-            <div class="panel panel-info">
+            <div class="panel panel-default">
                 <div class="panel-heading">Change Username</div>
                 <div class="panel-body">
-                    <form method="post" action="{{ url("/account/edit/username") }}">
+                    <div class="col-md-6">
+                        <form method="post" action="{{ url("/account/edit/username") }}">
 
-                        <div class="form-group">
-                            <label for="username">Username</label>
-                            <input type="text" id="username" name="username" class="form-control" placeholder="{{ $user->name }}" required>
-                        </div>
+                            <div class="form-group">
+                                <label for="username">Username</label>
+                                <input type="text" id="username" name="username" class="form-control" placeholder="{{ $user->name }}" required>
+                            </div>
 
-                        <div class="form-group">
-                            {{ csrf_field() }}
-                            {{ method_field("PATCH") }}
-                            <input type="submit" value="Edit" class="btn btn-info">
-                        </div>
-                    </form>
+                            <div class="form-group">
+                                {{ csrf_field() }}
+                                {{ method_field("PATCH") }}
+                                <input type="submit" value="Edit" class="btn btn-default">
+                            </div>
+                        </form>
+                    </div>
                 </div>
             </div>
         </div>
