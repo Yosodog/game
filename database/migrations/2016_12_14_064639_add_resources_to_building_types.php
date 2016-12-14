d9aafc7542b48e384aa4b26fb1f72a5b2577326f
Yosodog <yosodog@gmail.com>
2016-12-14 02:09:39 -0500

    Created building types resource migration and updated seeder (#4)

---
+++ database/migrations/2016_12_14_064639_add_resources_to_building_types.php
@@ -0,0 +1,42 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class AddResourcesToBuildingTypes extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table("building_types", function(Blueprint $table) {
+            $table->boolean("produces")->default(0)->comment("Does this building produce resources?");
+            $table->string("producedResource")->nullable()->comment("What resource does this building produce?");
+            $table->smallInteger("producedAmount")->nullable()->comment("How much does this building produce?");
+            $table->boolean("requires")->default(0)->comment("Does this building require resources to run?");
+            $table->string("requiredResource")->nullable()->comment("What resource does this building require?");
+            $table->smallInteger("requiredAmount")->nullable()->comment("What is the required amount for this building to run?");
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table("building_types", function(Blueprint $table) {
+            $table->dropColumn("produces");
+            $table->dropColumn("producedResource");
+            $table->dropColumn("producedAmount");
+            $table->dropColumn("requires");
+            $table->dropColumn("requiredResource");
+            $table->dropColumn("requiredAmount");
+        });
+    }
+}
