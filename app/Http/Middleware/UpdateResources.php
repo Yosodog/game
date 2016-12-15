f7d563354b0f4ae7d9a94f05f99a23a13063423c
Yosodog <yosodog@gmail.com>
2016-12-14 20:16:23 -0500

    Made buildings produce and require resources (#4)

--- app/Http/Middleware/UpdateResources.php
+++ app/Http/Middleware/UpdateResources.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Middleware;
 
+use App\Models\Nation\Building;
 use App\Models\Nation\Cities;
 use App\Models\Properties;
 use Closure;
@@ -77,6 +78,30 @@ class UpdateResources
     protected $steel = 0, $bauxite = 0, $aluminum = 0, $lead = 0, $ammo = 0;
 
     /**
+     * Holds the strings of the resources we want to update so later it makes updating things easier. You'll see
+     *
+     * @var array
+     */
+    protected $updates = [
+            "money", "coal", "oil", "gas", "rubber", "steel", "iron", "bauxite", "aluminum", "lead", "ammo",
+            "clay", "cement", "timber", "brick", "concrete", "lumber", "wheat", "livestock", "bread", "meat", "water"
+    ];
+
+    /**
+     * Holds the request for us
+     *
+     * @var \App\Http\Requests\
+     */
+    protected $request;
+
+    /**
+     * Used when the nation can only produce a % of a resource
+     *
+     * @var int|null
+     */
+    protected $percOfResource;
+
+    /**
      * Handle an incoming request.
      *
      * @param  \Illuminate\Http\Request  $request
@@ -85,6 +110,8 @@ class UpdateResources
      */
     public function handle($request, Closure $next)
     {
+        $this->request = $request;
+
         // Check if the user has a nation or not. If not, just go to the next thing
         if (Auth::guest() || !Auth::user()->hasNation)
             return $next($request);
@@ -156,7 +183,7 @@ class UpdateResources
     {
         // Calculate how much money they make per day
         $income = $city->properties["Avg Income"]["value"] * $city->population;
-        $this->money += round($income / 86400, 2);
+        $this->money += $income / 86400;
     }
 
     /**
@@ -166,7 +193,83 @@ class UpdateResources
      */
     protected function calcResources(Cities $city)
     {
+        foreach ($city->buildings as $building)
+        {
+            $this->percOfResource = null; // Always make sure this was reset
 
+            if (!$building->buildingType->produces && !$building->buildingType->requires)
+                continue; // If the building does not produce or require resources, just go to the next iteration
+
+            if ($building->buildingType->requires)
+                if (!$this->calcRequired($building))
+                    continue; // If the building does not have the required resource, move to next iteration
+
+            // Now that the required resources has been taken, produce the produced resources
+            if ($building->buildingType->produces)
+                $this->calcProduced($building);
+        }
+    }
+
+    /**
+     * Verify that the nation has the required resources and if so, subtract those resources
+     *
+     * @param Building $building
+     * @return bool
+     */
+    protected function calcRequired(Building $building) : bool
+    {
+        // Calculate the amount this building needs
+        $reqAmount = (($building->buildingType->requiredAmount * $building->quantity) / 86400) * $this->diff;
+
+        // Is the required amount more than what they have?
+        if ($reqAmount > $this->user->nation->resources->{$building->buildingType->requiredResource}) // holy shit
+        {
+            // We need to check if they had enough resources to produce some stuff.
+            // If they have nothing of what's required, just return false
+            if ($this->user->nation->resources->{$building->buildingType->requiredResource} == 0)
+                return false;
+
+            // if they have some of the required resource but not enough for the entire duration,
+            // calculate what percentage they can afford and later, that'll be how much they produce
+            $this->percOfResource = $this->user->nation->resources->{$building->buildingType->requiredResource} / $reqAmount;
+
+            // Now remove all of this resource. Divide it by the diff because later we multiply it by the diff. This makes it more simple
+            $this->{$building->buildingType->requiredResource} -= $this->user->nation->resources->{$building->buildingType->requiredResource} / $this->diff;
+
+            // Return true now that the resources has been subtracted
+            return true;
+        }
+
+        // If the nation has the required resource, then subtract it
+        $this->{$building->buildingType->requiredResource} -= $reqAmount;
+
+        // Also subtract it from the nation->resources variable so that the next buildings are checked properly
+        $this->user->nation->resources->{$building->buildingType->requiredResource} -= $reqAmount;
+
+        return true;
+    }
+
+    /**
+     * Calculates the amount this building should produce
+     *
+     * @param Building $building
+     */
+    protected function calcProduced(Building $building)
+    {
+        // Check if we should only produce a certain percentage
+        if ($this->percOfResource != null)
+        {
+            // Calculate how much they should produce if they had all required resource, then multiply by the percentage
+            $produce = ((($building->buildingType->producedAmount * $building->quantity) / 86400) * $this->diff) * $this->percOfResource;
+
+            // Set the amount needed but divide it by the the diff because later is it multiplied by the diff
+            $this->{$building->buildingType->producedResource} += $produce / $this->diff;
+
+            return;
+        }
+
+        // If they can produce the full amount, add it here
+        $this->{$building->buildingType->producedResource} += ($building->buildingType->producedAmount * $building->quantity) / 86400;
     }
 
     /**
@@ -176,7 +279,8 @@ class UpdateResources
     {
         $resources = $this->user->nation->resources;
 
-        $resources->money += $this->money * $this->diff;
+        foreach ($this->updates as $update)
+            $resources->{$update} += $this->{$update} * $this->diff;
 
         $resources->save();
     }
@@ -196,8 +300,7 @@ class UpdateResources
      */
     protected function updateSession()
     {
-        session([
-            "moneyPerSec" => $this->money,
-        ]);
+        foreach ($this->updates as $update)
+            $this->request->session()->put($update."PerSec", $this->{$update});
     }
 }
