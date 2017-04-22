cd793d9967f718beb67500fca5eb00498c2e06f6
LordStrum <lordstrum@yahoo.com>
2017-04-22 00:16:55 -0400

    added alliance permissions to game

---
+++ database/migrations/2017_04_22_034354_add_default_role_to_alliance_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class AddDefaultRoleToAllianceTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('alliances', function (Blueprint $table) {
+        	$table->integer('default_role_id')->unsigned()->nullable();
+        	
+        	$table->foreign('default_role_id')->references('id')->on('roles');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('alliances', function (Blueprint $table) {
+        	$table->dropForeign(['default_role_id']);
+        	$table->dropColumn('default_role_id');
+        });
+    }
+}
