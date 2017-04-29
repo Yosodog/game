c663a351bf450fadec4c32433d4da0c5c99096b4
smritz <shaneritz@gmail.com>
2017-04-29 16:47:56 -0500

    Added ability to search nations and alliances.

--- resources/views/alliance/all.blade.php
+++ resources/views/alliance/all.blade.php
@@ -3,6 +3,21 @@
 @section('content')
     @include("alerts") {{-- Include the template for alerts. This checks if there's something needed to display --}}
     <h1 class="text-center">Alliances</h1>
+
+    <div class="text-center">
+        <form method="post">
+            <div class="form-inline">
+                Search for <input type="text" class="form-control" id="search" name="search" value="@if(isset($search)){{$search}}@endif">
+                in <select class="form-control" id="category" name="category">
+                    <option value="everything">Everything</option>
+                    <option value="aName">Alliance Name</option>
+                </select>
+                <input type="submit" class="btn btn-primary" value="Search">
+            </div>
+            {{ csrf_field() }}
+        </form>
+    </div>
+
     <div class="text-center">
         {{ $alliances->links() }}
     </div>
