fe10397ee17771f972d554294f25bb94fb407e3c
Yosodog <yosodog@gmail.com>
2017-11-02 20:09:36 -0500

    Added unread messages count to navigation

--- resources/views/layouts/nav/account.blade.php
+++ resources/views/layouts/nav/account.blade.php
@@ -1,9 +1,9 @@
 <a href="#">
-    {{ Auth::user()->name }} <span class="caret"></span>
+    {{ Auth::user()->name }} @if(Auth::user()->newThreadsCount() > 0)<span class="badge">{{ Auth::user()->newThreadsCount() }}@endif</span> <span class="caret"></span>
 </a>
 
 <ul class="dropdown-menu" role="menu">
-    <li><a href="{{ url('/account/inbox') }}"></i>Messages</a></li>
+    <li><a href="{{ url('/account/inbox') }}"></i>Messages @if(Auth::user()->newThreadsCount() > 0)<span class="badge">{{ Auth::user()->newThreadsCount() }}@endif</a></li>
     <li><a href="{{ url('#') }}"></i>Notifications</a></li>
     <li><a href="{{ url('/account') }}">Account</a></li>
     <li><a href="{{ url('/logout') }}"><i class="fa fa-btn fa-sign-out"></i>Logout</a></li>
