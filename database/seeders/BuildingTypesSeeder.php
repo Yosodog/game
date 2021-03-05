645448594f709e21fcbba26ea3a1b94604411bbb
Yosodog <yosodog@gmail.com>
2021-03-05 15:18:58 -0600

    Update to Laravel 8

--- database/seeders/BuildingTypesSeeder.php
+++ database/seeders/BuildingTypesSeeder.php
@@ -1,5 +1,7 @@
 <?php
 
+namespace Database\Seeders;
+
 use \App\Models\Effects;
 use \App\Models\Properties;
 use \App\Models\BuildingTypes;
--- database/seeders/PropertiesSeeder.php
+++ database/seeders/PropertiesSeeder.php
@@ -1,5 +1,7 @@
 <?php
 
+namespace Database\Seeders;
+
 use \App\Models\Properties;
 use Illuminate\Database\Seeder;
 
